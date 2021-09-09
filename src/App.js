c70e29faceb36537a30bdab6c0386674cf2a9e0a
bujiangfeng <bujiangfeng@58.com>
2021-09-09 22:21:40 +0800

    init基座

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import microAppList from './microAppList';
+
+const style = {
+  marginRight: "20px"
+}
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Link to="/" style={style}>Home</Link>
+        {microAppList.map((item) => {
+          return <Link to={item.activeRule} key={item.name} style={style}>{item.name}</Link>
+        })}
+      </Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          这就是个基座
+        </div>
       </header>
+      <div id="subapp" ></div>
     </div>
   );
 }
---
+++ src/microAppList.js
@@ -0,0 +1,28 @@
+let microAppList = [
+    {
+        name: 'subreact', // apppName 需要和子系统wepack配置的library对应
+        // container: '#subapp-viewport', // 显示子应用的容器
+        entry: process.env.REACT_APP_SUBREACT, // 加载微应用url地址
+        activeRule: '/subreact', // 触发路由
+        props: {} // 自定义属性
+    },
+    {
+        name: 'subvue',
+        entry: process.env.REACT_APP_SUBVUE,
+        activeRule: '/subvue',
+        props: {}
+    },
+    {
+        name: 'subhtml',
+        entry: process.env.REACT_APP_SUBHTML,
+        activeRule: '/subhtml',
+        props: {}
+    }
+]
+microAppList = microAppList.map((item)=>{
+    return Object.assign({container:"#subapp"},item)
+})
+
+console.log(microAppList);
+
+export default microAppList
