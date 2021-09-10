b0b9746c884117eccb9ec1492e3831fbeac11930
bujiangfeng <bujiangfeng@58.com>
2021-09-10 16:24:39 +0800

    修改路由

--- src/microAppList.js
+++ src/microAppList.js
@@ -3,19 +3,19 @@ let microAppList = [
         name: 'subreact', // apppName 需要和子系统wepack配置的library对应
         // container: '#subapp-viewport', // 显示子应用的容器
         entry: process.env.REACT_APP_SUBREACT, // 加载微应用url地址
-        activeRule: '/subreact', // 触发路由
+        activeRule: '/micro/subreact', // 触发路由
         props: {} // 自定义属性
     },
     {
         name: 'subvue',
         entry: process.env.REACT_APP_SUBVUE,
-        activeRule: '/subvue',
+        activeRule: '/micro/subvue',
         props: {}
     },
     {
         name: 'subhtml',
         entry: process.env.REACT_APP_SUBHTML,
-        activeRule: '/subhtml',
+        activeRule: '/micro/subhtml',
         props: {}
     }
 ]
