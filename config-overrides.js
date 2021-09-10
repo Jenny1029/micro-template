04d771a864ec9e75b6ac36ac5444c72610d73b6b
bujiangfeng <bujiangfeng@58.com>
2021-09-10 15:57:46 +0800

    修改打包路径

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,8 @@
 /* config-overrides.js */
+const path = require("path");
+
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
+    config.output.path = path.join(path.dirname(config.output.path), "dist");
     return config;
 }
