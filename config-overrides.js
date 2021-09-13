6eb052d7c61d943052dbb4e05adfff877e13e4c6
bujiangfeng <bujiangfeng@58.com>
2021-09-13 17:53:11 +0800

    修改打包文件名

--- config-overrides.js
+++ config-overrides.js
@@ -8,6 +8,6 @@ module.exports = function override(config, env) {
     const paths = require('react-scripts/config/paths');
     paths.appBuild = path.join(path.dirname(paths.appBuild), 'dist');
     config.output.path = path.join(path.dirname(config.output.path), 'dist');
-    console.log(`custom output path - ${ config.output.path }`);
+    config.output.filename = 'static/js/microbasic.js';
     return config;
 }
